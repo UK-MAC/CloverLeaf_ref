97c9ffa15bae0675bc0bbdf579330084d5f0940c
A.Mallinson <acm@dcs.warwick.ac.uk>
2013-12-24 19:06:42 +0000

    updated buildfield to potentially improve the first touch memory allocation
    needs testing, not compiled yet

--- build_field.f90
+++ build_field.f90
@@ -90,6 +90,7 @@ SUBROUTINE build_field(chunk,x_cells,y_cells)
    ALLOCATE(chunks(chunk)%field%celldy  (chunks(chunk)%field%y_min-2:chunks(chunk)%field%y_max+2))
    ALLOCATE(chunks(chunk)%field%vertexdx(chunks(chunk)%field%x_min-2:chunks(chunk)%field%x_max+3))
    ALLOCATE(chunks(chunk)%field%vertexdy(chunks(chunk)%field%y_min-2:chunks(chunk)%field%y_max+3))
+
    ALLOCATE(chunks(chunk)%field%volume  (chunks(chunk)%field%x_min-2:chunks(chunk)%field%x_max+2, &
                                          chunks(chunk)%field%y_min-2:chunks(chunk)%field%y_max+2))
    ALLOCATE(chunks(chunk)%field%xarea   (chunks(chunk)%field%x_min-2:chunks(chunk)%field%x_max+3, &
@@ -100,44 +101,87 @@ SUBROUTINE build_field(chunk,x_cells,y_cells)
    ! Zeroing isn't strictly neccessary but it ensures physical pages
    ! are allocated. This prevents first touch overheads in the main code
    ! cycle which can skew timings in the first step
+
 !$OMP PARALLEL
-   chunks(chunk)%field%work_array1=0.0
-   chunks(chunk)%field%work_array2=0.0
-   chunks(chunk)%field%work_array3=0.0
-   chunks(chunk)%field%work_array4=0.0
-   chunks(chunk)%field%work_array5=0.0
-   chunks(chunk)%field%work_array6=0.0
-   chunks(chunk)%field%work_array7=0.0
+!$OMP DO 
+    DO k=chunks(chunk)%field%y_min-2,chunks(chunk)%field%y_max+3
+        DO j=chunks(chunk)%field%x_min-2,chunks(chunk)%field%x_max+3
+            chunks(chunk)%field%work_array1=0.0
+            chunks(chunk)%field%work_array2=0.0
+            chunks(chunk)%field%work_array3=0.0
+            chunks(chunk)%field%work_array4=0.0
+            chunks(chunk)%field%work_array5=0.0
+            chunks(chunk)%field%work_array6=0.0
+            chunks(chunk)%field%work_array7=0.0
 
-   chunks(chunk)%field%density0=0.0
-   chunks(chunk)%field%density1=0.0
-   chunks(chunk)%field%energy0=0.0
-   chunks(chunk)%field%energy1=0.0
-   chunks(chunk)%field%pressure=0.0
-   chunks(chunk)%field%viscosity=0.0
-   chunks(chunk)%field%soundspeed=0.0
-   
-   chunks(chunk)%field%xvel0=0.0
-   chunks(chunk)%field%xvel1=0.0
-   chunks(chunk)%field%yvel0=0.0
-   chunks(chunk)%field%yvel1=0.0
-   
-   chunks(chunk)%field%vol_flux_x=0.0
-   chunks(chunk)%field%mass_flux_x=0.0
-   chunks(chunk)%field%vol_flux_y=0.0
-   chunks(chunk)%field%mass_flux_y=0.0
+            chunks(chunk)%field%xvel0=0.0
+            chunks(chunk)%field%xvel1=0.0
+            chunks(chunk)%field%yvel0=0.0
+            chunks(chunk)%field%yvel1=0.0
+        ENDDO
+    ENDDO
+!$OMP END DO
 
-   chunks(chunk)%field%cellx=0.0
-   chunks(chunk)%field%celly=0.0
-   chunks(chunk)%field%vertexx=0.0
-   chunks(chunk)%field%vertexy=0.0
-   chunks(chunk)%field%celldx=0.0
-   chunks(chunk)%field%celldy=0.0
-   chunks(chunk)%field%vertexdx=0.0
-   chunks(chunk)%field%vertexdy=0.0
-   chunks(chunk)%field%volume=0.0
-   chunks(chunk)%field%xarea=0.0
-   chunks(chunk)%field%yarea=0.0
+!$OMP DO 
+    DO k=chunks(chunk)%field%y_min-2,chunks(chunk)%field%y_max+2
+        DO j=chunks(chunk)%field%x_min-2,chunks(chunk)%field%x_max+2
+            chunks(chunk)%field%density0=0.0
+            chunks(chunk)%field%density1=0.0
+            chunks(chunk)%field%energy0=0.0
+            chunks(chunk)%field%energy1=0.0
+            chunks(chunk)%field%pressure=0.0
+            chunks(chunk)%field%viscosity=0.0
+            chunks(chunk)%field%soundspeed=0.0
+            chunks(chunk)%field%volume=0.0
+        ENDDO
+    ENDDO
+!$OMP END DO
+
+!$OMP DO 
+    DO k=chunks(chunk)%field%y_min-2,chunks(chunk)%field%y_max+2  
+        DO j=chunks(chunk)%field%x_min-2,chunks(chunk)%field%x_max+3  
+            chunks(chunk)%field%vol_flux_x=0.0
+            chunks(chunk)%field%mass_flux_x=0.0
+            chunks(chunk)%field%xarea=0.0
+        ENDDO
+    ENDDO
+!$OMP END DO
+!$OMP DO 
+    DO k=chunks(chunk)%field%y_min-2,chunks(chunk)%field%y_max+3
+        DO j=chunks(chunk)%field%x_min-2,chunks(chunk)%field%x_max+2
+            chunks(chunk)%field%vol_flux_y=0.0
+            chunks(chunk)%field%mass_flux_y=0.0
+            chunks(chunk)%field%yarea=0.0
+        ENDDO
+    ENDDO
+!$OMP END DO
+
+!$OMP DO 
+    DO j=chunks(chunk)%field%x_min-2,chunks(chunk)%field%x_max+2
+        chunks(chunk)%field%cellx=0.0
+        chunks(chunk)%field%celldx=0.0
+    ENDDO
+!$OMP END DO
+!$OMP DO 
+    DO k=chunks(chunk)%field%y_min-2,chunks(chunk)%field%y_max+2
+        chunks(chunk)%field%celly=0.0
+        chunks(chunk)%field%celldy=0.0
+    ENDDO
+!$OMP END DO
+
+!$OMP DO 
+    DO j=chunks(chunk)%field%x_min-2,chunks(chunk)%field%x_max+3
+        chunks(chunk)%field%vertexx=0.0
+        chunks(chunk)%field%vertexdx=0.0
+    ENDDO
+!$OMP END DO
+!$OMP DO 
+    DO k=chunks(chunk)%field%y_min-2,chunks(chunk)%field%y_max+3
+        chunks(chunk)%field%vertexy=0.0
+        chunks(chunk)%field%vertexdy=0.0
+    ENDDO
+!$OMP END DO
+
 !$OMP END PARALLEL
   
 END SUBROUTINE build_field
