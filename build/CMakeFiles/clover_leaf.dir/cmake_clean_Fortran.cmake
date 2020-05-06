9e4093c80cbdb8ac33497569ad0d217c1e8d386c
Wei <Wei.Liu_UK@protonmail.com>
2020-05-06 15:44:38 +0100

    Initial CMake changes

---
+++ build/CMakeFiles/clover_leaf.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,150 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "accelerate_kernel_module.mod"
+  "ACCELERATE_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/accelerate_kernel_module.mod.stamp"
+
+  "accelerate_module.mod"
+  "ACCELERATE_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/accelerate_module.mod.stamp"
+
+  "advec_cell_driver_module.mod"
+  "ADVEC_CELL_DRIVER_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/advec_cell_driver_module.mod.stamp"
+
+  "advec_cell_kernel_module.mod"
+  "ADVEC_CELL_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/advec_cell_kernel_module.mod.stamp"
+
+  "advec_mom_driver_module.mod"
+  "ADVEC_MOM_DRIVER_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/advec_mom_driver_module.mod.stamp"
+
+  "advec_mom_kernel_mod.mod"
+  "ADVEC_MOM_KERNEL_MOD.mod"
+  "CMakeFiles/clover_leaf.dir/advec_mom_kernel_mod.mod.stamp"
+
+  "advection_module.mod"
+  "ADVECTION_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/advection_module.mod.stamp"
+
+  "calc_dt_kernel_module.mod"
+  "CALC_DT_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/calc_dt_kernel_module.mod.stamp"
+
+  "calc_dt_module.mod"
+  "CALC_DT_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/calc_dt_module.mod.stamp"
+
+  "clover_case_change.mod"
+  "CLOVER_CASE_CHANGE.mod"
+  "CMakeFiles/clover_leaf.dir/clover_case_change.mod.stamp"
+
+  "clover_isitanint_mod.mod"
+  "CLOVER_ISITANINT_MOD.mod"
+  "CMakeFiles/clover_leaf.dir/clover_isitanint_mod.mod.stamp"
+
+  "clover_module.mod"
+  "CLOVER_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/clover_module.mod.stamp"
+
+  "data_module.mod"
+  "DATA_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/data_module.mod.stamp"
+
+  "definitions_module.mod"
+  "DEFINITIONS_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/definitions_module.mod.stamp"
+
+  "field_summary_kernel_module.mod"
+  "FIELD_SUMMARY_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/field_summary_kernel_module.mod.stamp"
+
+  "flux_calc_kernel_module.mod"
+  "FLUX_CALC_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/flux_calc_kernel_module.mod.stamp"
+
+  "flux_calc_module.mod"
+  "FLUX_CALC_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/flux_calc_module.mod.stamp"
+
+  "generate_chunk_kernel_module.mod"
+  "GENERATE_CHUNK_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/generate_chunk_kernel_module.mod.stamp"
+
+  "ideal_gas_kernel_module.mod"
+  "IDEAL_GAS_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/ideal_gas_kernel_module.mod.stamp"
+
+  "ideal_gas_module.mod"
+  "IDEAL_GAS_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/ideal_gas_module.mod.stamp"
+
+  "initialise_chunk_kernel_module.mod"
+  "INITIALISE_CHUNK_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/initialise_chunk_kernel_module.mod.stamp"
+
+  "pack_kernel_module.mod"
+  "PACK_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/pack_kernel_module.mod.stamp"
+
+  "parse_module.mod"
+  "PARSE_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/parse_module.mod.stamp"
+
+  "pdv_kernel_module.mod"
+  "PDV_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/pdv_kernel_module.mod.stamp"
+
+  "pdv_module.mod"
+  "PDV_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/pdv_module.mod.stamp"
+
+  "report_module.mod"
+  "REPORT_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/report_module.mod.stamp"
+
+  "reset_field_kernel_module.mod"
+  "RESET_FIELD_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/reset_field_kernel_module.mod.stamp"
+
+  "reset_field_module.mod"
+  "RESET_FIELD_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/reset_field_module.mod.stamp"
+
+  "revert_kernel_module.mod"
+  "REVERT_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/revert_kernel_module.mod.stamp"
+
+  "revert_module.mod"
+  "REVERT_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/revert_module.mod.stamp"
+
+  "timestep_module.mod"
+  "TIMESTEP_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/timestep_module.mod.stamp"
+
+  "update_halo_kernel_module.mod"
+  "UPDATE_HALO_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/update_halo_kernel_module.mod.stamp"
+
+  "update_halo_module.mod"
+  "UPDATE_HALO_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/update_halo_module.mod.stamp"
+
+  "update_tile_halo_kernel_module.mod"
+  "UPDATE_TILE_HALO_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/update_tile_halo_kernel_module.mod.stamp"
+
+  "update_tile_halo_module.mod"
+  "UPDATE_TILE_HALO_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/update_tile_halo_module.mod.stamp"
+
+  "viscosity_kernel_module.mod"
+  "VISCOSITY_KERNEL_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/viscosity_kernel_module.mod.stamp"
+
+  "viscosity_module.mod"
+  "VISCOSITY_MODULE.mod"
+  "CMakeFiles/clover_leaf.dir/viscosity_module.mod.stamp"
+  )
